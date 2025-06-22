0a757503721bd0aa39ae10f516c4daf0ba97f739
James Olando <jolandomolo@gmail.com>
2025-06-22 16:22:18 -0700

    Connecting to the database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv"
 
+import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
+
 
 dotenv.config()
 const app = express();
@@ -12,4 +15,5 @@ app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
     console.log("server is running on PORT:" + PORT);
+    connectDB();
 });
