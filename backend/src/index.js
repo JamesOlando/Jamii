94748299d946c2475d0b7b951f11fd370c493ffe
James Olando <jolandomolo@gmail.com>
2025-06-22 18:06:23 -0700

    Setting up the signup including authentication

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,11 +5,12 @@ import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 
-
 dotenv.config()
 const app = express();
 
 const PORT = process.env.PORT
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes)
 
