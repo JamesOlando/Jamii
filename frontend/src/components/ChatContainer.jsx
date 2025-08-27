1ab84607a5cdae2b9280edc05478c0e6ca7f6304
James Olando <jolandomolo@gmail.com>
2025-08-26 18:20:49 -0700

    setting up the chat container: MessageInput

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
 
 const ChatContainer = () => {
   const { messages, getMessages, isMessagesLoading, selectedUser } = useChatStore();
@@ -12,7 +13,15 @@ const ChatContainer = () => {
     getMessages(selectedUser._id)
   }, [selectedUser._id, getMessages])
 
-  if(isMessagesLoading) return <div>Loading...</div>
+  if(isMessagesLoading) {
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+       <ChatHeader />
+       <MessageSkeleton />
+       <MessageInput />
+      </div>
+    )
+  }
 
   
 
